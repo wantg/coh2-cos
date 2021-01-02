914c53e70e05c220baeb735343b47fbc8f4e5d5c
clintwan <wantg78@gmail.com>
2021-01-03 02:30:26 +0800

    add Rust Tauri

---
+++ tauri/README.md
@@ -0,0 +1,24 @@
+# Requirement
+
+## Setup for Windows
+
+https://tauri.studio/en/docs/getting-started/setup-windows
+
+
+# Run dev
+
+```
+$ npm run tauri dev
+```
+
+# Build
+
+```
+$ npm run tauri build --release
+```
+
+# WixTools
+
+downloaded https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip
+
+uncompress to src-tauri/WixTools
---
+++ tauri/src-tauri/src/main.rs
@@ -0,0 +1,76 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+mod cmd;
+
+use std::process::Command;
+use std::thread;
+
+fn start_hq(){
+  
+  let output = if cfg!(target_os = "windows") {
+    Command::new("hq\\hq.exe")
+            .args(&["-c", "config.yml"])
+            .output()
+            .expect("failed to execute process")
+  } else {
+    Command::new("sh")
+            .arg("-c")
+            .arg("echo hello")
+            .output()
+            .expect("failed to execute process")
+  };
+
+  println!("status: {}", output.status);
+  println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+  println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+}
+
+fn start_hq1(){
+
+  const NTHREADS: u32 = 10;
+  let mut children = vec![];
+
+  for i in 0..NTHREADS {
+      // Spin up another thread
+      children.push(thread::spawn(move || {
+          println!("this is thread number {}", i);
+      }));
+  }
+
+  for child in children {
+      // Wait for the thread to finish. Returns a result.
+      let _ = child.join();
+  }
+
+}
+
+fn main() {
+  thread::spawn(move || { start_hq(); });
+
+  println!("{}", "main.rs");
+  tauri::AppBuilder::new()
+    .invoke_handler(|_webview, arg| {
+      use cmd::Cmd::*;
+      match serde_json::from_str(arg) {
+        Err(e) => {
+          Err(e.to_string())
+        }
+        Ok(command) => {
+          match command {
+            // definitions for your custom commands from Cmd here
+            MyCustomCommand { argument } => {
+              //  your command code
+              println!("{}", argument);
+            }
+          }
+          Ok(())
+        }
+      }
+    })
+    .build()
+    .run();
+}
